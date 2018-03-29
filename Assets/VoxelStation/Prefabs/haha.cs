b53a7d016f5aa801fcd25454ece4e4b0ecc7888c
bin <405933039@qq.com>
2018-03-29 18:04:07 +0800

    update script haha

--- Assets/VoxelStation/Prefabs/haha.cs
+++ Assets/VoxelStation/Prefabs/haha.cs
@@ -7,6 +7,7 @@ class DecryptUtility
     [MenuItem("Utility/DecryptFile")]
     static void DecryptFile()
     {
+        Debug.Log("haha");
         var inputFile = "D:/Downloads/15e4fdfb-eb07-4dc1-852a-dcb9b6562127";
         var key = "65937b7b4505dfae07d95cd61e7987c994ace247ee7518eecdc711e8bd60bae8a0ed2177b66e9f690627683d697b33fe";
 
